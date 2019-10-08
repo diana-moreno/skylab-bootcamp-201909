1b1d2bd71e97dc979d239a31bc9bcdd762d0aef4
Albert <al.ferrer.artiaga@gmail.com>
2019-10-08 13:11:58 +0200

    add concat changes

--- staff/albert-ferrer/array-functions/concat.js
+++ staff/albert-ferrer/array-functions/concat.js
@@ -1,42 +1,11 @@
 /**
  * Used to merge two or more arrays. Not change the existing arrays, but instead returns a new array.
  * 
- * 
+ * @param {Array} array 
  *  
  * 
  */
 
-array1 = ['a', 'b', 'c'];
-array2 = ['d', 'e', 'f'];
-array3 = ['g', 'h', 'i'];
-array4 = [12,45];
-/*
-function concat(x,y,z,e,f) {
-    var arrays = x;
-    var contador= x.length;
-    for (let j = 0; j < y.length; j++) {
-        arrays[contador] = y[j];
-        contador++;
-    }
-
-    return arrays;
-}
-concat(array1,array2);
-
-
-function concat(x) {
-    var arrays = x;
-    var contador = x.length;
-    for (let i = 1; i < arguments.length; i++) {
-        for (let j = 0; j < arguments[i].length; j++) {
-            arrays[contador] = arguments[i][j];
-            contador++;
-        }
-    }
-    return arrays;
-}
-concat(array1, array2, array3);
-*/
 function concat(x) {
     var arrays = x;
     var contador = x.length;
@@ -55,4 +24,3 @@ function concat(x) {
     }
 }
 
-concat(array1, array2, array3, array4);
