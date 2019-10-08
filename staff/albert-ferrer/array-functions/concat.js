1fb075c0b568f1f483c9125f7048f53ad49c43f8
Albert <al.ferrer.artiaga@gmail.com>
2019-10-08 13:28:07 +0200

    add last coments

--- staff/albert-ferrer/array-functions/concat.js
+++ staff/albert-ferrer/array-functions/concat.js
@@ -1,14 +1,16 @@
 /**
  * Used to merge two or more arrays. Not change the existing arrays, but instead returns a new array.
  * 
- * @param {Array} array 
+ * @param {Array} array Initial Array with which the other arrays and/or values will be attached.
+ * 
+ * @param {Function} expression Complementary array and/or values concatenated to the initial array.Complementary array and/or values concatenated to the initial array.
  *  
  * 
  */
 
-function concat(x) {
-    var arrays = x;
-    var contador = x.length;
+function concat(a,b,c,d) {
+    var arrays = a;
+    var contador = a.length;
     for (let i = 1; i < arguments.length; i++) {
         if (arguments[i] instanceof Array) {
             for (let j = 0; j < arguments[i].length; j++) {
