375ac034a781d442ab6676d1e928b2efbae330f2
Albert <al.ferrer.artiaga@gmail.com>
2019-10-12 12:08:16 +0200

    unshift

--- staff/albert-ferrer/array-functions/unshift.js
+++ staff/albert-ferrer/array-functions/unshift.js
@@ -1,8 +1,8 @@
 numbers = [1,2,3,4,5];
 
 function unshift(array){ 
-    for (var i = 4; i > arguments.length; i--){
-        array[arguments.length+1]=array[i];
+    for (var i = array.length-1; i < 0; i--){
+        array[i + arguments.length-1]=array[i];
     }
 }
 unshift(numbers, 6);
