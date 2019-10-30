c9c1d70cce613929b56aa1f969ae8478f44f54df
Javier Sevilla <xaviersevilla@gmail.com>
2019-10-30 18:35:57 +0100

    update detail end

--- staff/groups/groups/chapu-n-defined/eat-the-world/eat-the-world-app/components/detail/index.jsx
+++ staff/groups/groups/chapu-n-defined/eat-the-world/eat-the-world-app/components/detail/index.jsx
@@ -1,40 +1,32 @@
-function Detail({ restaurant }) {
-document.getElementByClassName("detail__header-container").style.backgroundSize = "cover"
-document.getElementByClassName("detail__header-container").style.backgroundImage = "url(" + restaurant.url + ")"
-document.getElementByClassName("detail__header-container").style.backgroundPosition = "center" 
-  
+function Detail({ restaurant }) { 
   return (
     <section className='detail'>
       <div className='detail__main-container'>
         <h1 className='detail__title'>{restaurant.name}</h1>
         <div className='detail__header-container'>
-          {/* <img src={restaurant.featured_image}/> */}
-          <img src={}/>
+          <img className = "detail__img" src={restaurant.featured_image}/>
           <p className='detail__puntuation'>{restaurant.user_rating.aggregate_rating}</p>
         </div>
         <div className='detail__columns-container'>
           <div className='detail__column-container'>
-            <h2 className='detail__detail-title'>Tipo de cocina</h2>
+            <h2 className='detail__detail-title'>Cuisine type</h2>
             <p className='detail__cuisine'>{restaurant.cuisines}</p>
-            <h2 className='detail__detail-title'>Dirección</h2>
+            <h2 className='detail__detail-title'>Adress</h2>
             <p className='detail__direction'>{restaurant.location.address}</p>
 
-            <h2 className='detail__detail-title'>Teléfono</h2>
+            <h2 className='detail__detail-title'>Phone</h2>
             <p className='detail__telephone'>{restaurant.phone_numbers}</p>
           </div>
           <div className='detail__column-container'>
-            <h2 className='detail__detail-title'>Precio medio</h2>
+            <h2 className='detail__detail-title'>Average cost for two</h2>
             <p className='detail__price'>{restaurant.costcurrency}</p>
-            <h2 className='detail__detail-title'>Establecimiento</h2>
+            <h2 className='detail__detail-title'>Establishment</h2>
             <p className='detail__web' href="">{restaurant.establishment}</p>
-            <h2 className='detail__detail-title'>Horario</h2>
+            <h2 className='detail__detail-title'>Business hours</h2>
             <p className='detail__time'>{restaurant.timings}</p>
           </div>
           <div className='detail__column-container detail__featured'>
-            <h2 className='detail__detail-title'>Destacados</h2>
-            {/* {restaurant.highlights.map((value) => {
-               <p>{value}</p>
-              })} */}
+            <h2 className='detail__detail-title'>Highlights</h2>
               <p>{restaurant.highlights[0]}</p>
               <p>{restaurant.highlights[1]}</p>
               <p>{restaurant.highlights[2]}</p>
