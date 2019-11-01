5af1578120b32dd31687671593d72fedcb25dcda
Diana <d7@hotmail.es>
2019-11-01 11:06:48 +0100

    documentate authenticate-user logic

--- staff/groups/groups/chapu-n-defined/eat-the-world/eat-the-world-app/logic/authenticate-user/index.js
+++ staff/groups/groups/chapu-n-defined/eat-the-world/eat-the-world-app/logic/authenticate-user/index.js
@@ -1,3 +1,12 @@
+/**
+ * Makes a call to an API, sending email and password, to retrieve other personal data from the user as id and token
+ * @param  {String}   email    recibes a user email
+ * @param  {String}   password recibes a user password
+ * @param  {Function} callback recibes a callback to retrieve the results of the call to the API
+ * @throws {TypeError}    If email or password is not a number or a string
+ * @throws {TypeError}    If callback is not a function
+ * @throws {ContnetError}    If email or password is empty or blank
+ */
 function authenticateUser(email, password, callback) {
   if(typeof email !== 'string') throw new TypeError(email + ' is not a string')
   if(!email.trim().length) throw new ContentError('e-mail is empty or blank')
