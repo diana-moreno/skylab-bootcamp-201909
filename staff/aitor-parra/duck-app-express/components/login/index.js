005c10afb7098fca89935774e61ce514b75edfbc
Aitor Parra <aitor1979@protonmail.com>
2019-11-06 18:00:53 +0100

    first duck-app-express

---
+++ staff/aitor-parra/duck-app-express/components/login/index.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+
+    return `<section class="view login">
+    <form>
+        <h1 class="login__title">Login</h1>
+        <input class="login__field" type="email" name="email" placeholder="e-mail">
+        <input class="login__field" type="password" name="password" placeholder="password">
+        <button class="login__submit">🗝</button>
+    </form>
+    </section>`
+
+}
