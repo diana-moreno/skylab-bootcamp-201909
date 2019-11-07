6e5fb5708a2c96a01e7e024b6e013a9d39d42d46
OscarErre <oscarr.rodfer@gmail.com>
2019-11-07 17:06:14 +0100

    nose

--- staff/oscar-rodriguez/duck-app-express/components/search/index.js
+++ staff/oscar-rodriguez/duck-app-express/components/search/index.js
@@ -6,7 +6,7 @@ module.exports = function ({name, path, query}) {
     <h2 class="search__user">${name}</h2><button class="search__logout">Logout</button>
     <form class="search__form" method="GET" action="${path}">
         <span class="search__icon">🐣</span>
-        <input class="search__criteria" type="text" name="query" placeholder="criteria">
+        <input class="search__criteria" type="text" name="query" placeholder="criteria" ${query? `value=${query}` : ''}>
         <button class="search__submit">🔍</button>
     </form>
 </section>`
--- staff/oscar-rodriguez/duck-app-express/index.js
+++ staff/oscar-rodriguez/duck-app-express/index.js
@@ -41,11 +41,11 @@ app.post('/register', (req,res)=> {
 
         try {
             registerUser (name, surname, email, password, error => {
-                if (error) return res.send(error)
+                if (error) return res.send(error.message)
                 res.redirect('/')
             })
         } catch (error) {
-            res.send(error)
+            res.send(error.message)
         }
     })
 })
@@ -63,27 +63,49 @@ app.post('/login', (req,res)=> {
 
         try {
             authenticateUser (email, password, (error, result) => {
-                if (error) return res.send(error)
-                id = result.id
-                token = result.token
-                try {
-                    retrieveUser (id, token, (error,result)=>{
-                        username = result.data.name
-                        res.redirect('./search')
-                    })
-                } catch (error) {res.send(error)}
+                if (error) return res.send(error.message)
+
+                const {id,token} = CredentialsContainer
+                sessions[id] = token
+
+                res.setHeader('set-cookie', `id=${id}`)
+                res.redirect('./search')
             })
         } catch (error) {
-            res.send(error)
+            res.send(error.message)
         }
     })
 })
 
 app.get('/search', (req, res)=>{
+    try {
+        const { headers: { cookie } } = req
+        if (!cookie) return res.redirect('/')
 
-    const { query : { q }} = req
-    if (!q) res.send(View({ body: Search({name: username, path: '/search'}) }))
-    else 
+        const [, id] = cookie.split('id=')
+
+        const token = sessions[id]
+
+        if (!token) return res.redirect('/')
+
+        
+        retrieveUser (id, token, (error, user) => {
+            if (error) return res.send (error.message)
+            
+            const { name } = user
+            
+            const { query : { q: query }} = req
+
+            if (!query) res.send(View({ body: Search({name: username, path: '/search'}) }))
+            else {
+                try {
+        
+                } catch (error) {}
+        
+            }
+        })
+    } catch (error) { res.send (error.message)}
+
 })
 
 app.get('/results', (req, res)=> {
