05c34dad1d8ab67d82c470afb4f2d90dedc719c0
Diana <d7@hotmail.es>
2019-11-08 19:09:03 +0100

    add favorite hearts

--- staff/diana-moreno/duck-app-express/components/results-item/index.js
+++ staff/diana-moreno/duck-app-express/components/results-item/index.js
@@ -1,10 +1,33 @@
 module.exports = function ({ item: { id, title, imageUrl, price, isFav }, favPath }) {
   return(
     `<li class="duck duck--clicked">
-      <i></i>
+      <i class="${isFav
+        ? 'duck__favorite fas fa-heart'
+        : 'duck__favorite far fa-heart' }">
+      </i>
       <h1 class='duck__title'>${title}</h1>
       <img class='duck__image' src="${imageUrl}"/>
       <button class='duck__button'>${price}</button>
     </li>`
   )
 }
+
+
+
+/*
+React:
+function ResultsItem({ duck, item, add, handleFavorite }) {
+  return(
+    <li className="duck duck--clicked">
+      <i className={duck.isFav
+                    ? "duck__favorite fas fa-heart"
+                    : 'duck__favorite far fa-heart'}
+      onClick={event => handleFavorite(duck.id)}></i>
+      <h1 className='duck__title'>{duck.title}</h1>
+      <img onClick={event => item(duck)}
+        className='duck__image' src={duck.imageUrl}/>
+      <button className='duck__button'>{duck.price}</button>
+    </li>
+  )
+}
+*/
