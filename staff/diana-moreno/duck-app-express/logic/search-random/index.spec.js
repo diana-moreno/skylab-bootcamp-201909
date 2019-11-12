1ae385b834a9e054a3a9f8962496471170aed414
Diana <d7@hotmail.es>
2019-11-12 22:20:33 +0100

    add test

--- staff/diana-moreno/duck-app-express/logic/search-random/index.spec.js
+++ staff/diana-moreno/duck-app-express/logic/search-random/index.spec.js
@@ -58,6 +58,58 @@ describe('logic - search random ducks', () => {
       })
   })
 
+  it('should return the specified number of ducks', () => {
+    num = 2
+    return searchRandom(id, token, num)
+      .then(ducks => {
+        expect(ducks).to.exist
+        expect(ducks.length).to.be.greaterThan(0)
+        expect(ducks.length).to.be.equal(num)
+
+        ducks.forEach(function(duck) {
+          expect(duck).to.exist
+          expect(typeof duck.id).to.equal('string')
+          expect(duck.id.length).to.be.greaterThan(0)
+
+          expect(duck.title).to.exist
+          expect(typeof duck.title).to.equal('string')
+          expect(duck.title.length).to.be.greaterThan(0)
+
+          expect(duck.imageUrl).to.exist
+          expect(typeof duck.imageUrl).to.equal('string')
+          expect(duck.imageUrl.length).to.be.greaterThan(0)
+
+          expect(duck.price).to.exist
+          expect(typeof duck.price).to.equal('string')
+          expect(duck.price.length).to.be.greaterThan(0)
+
+          expect(duck.isFav).to.be.false
+        })
+      })
+  })
+
+  it('the ducks returned are random', () => {
+    num = 6
+    let initialducks
+    searchRandom(id, token, num)
+      .then(ducks => {
+        initialducks = ducks
+        expect(ducks).to.exist
+        expect(ducks.length).to.be.greaterThan(0)
+        expect(ducks.length).to.be.equal(num)
+      })
+      .then((ducks) => {
+        searchRandom(id, token, num)
+        .then(ducks2 => {
+          expect(ducks2).to.exist
+          expect(ducks2.length).to.be.greaterThan(0)
+          expect(ducks2.length).to.be.equal(num)
+          expect(initialducks).not.equal(ducks2)
+          expect(initialducks[0]).not.equal(ducks2[0])
+        })
+      })
+  })
+
   it('should fail on incorrect type number', () => {
     // TODO cases when id and token have values diff from non-empty string
 
