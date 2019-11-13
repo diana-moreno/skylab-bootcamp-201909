31d73ee3540cdb6ec4663cd7a05072f1275a926a
Diana <d7@hotmail.es>
2019-11-13 13:48:29 +0100

    add code

---
+++ staff/diana-moreno/tasks/tasks-api/data/users/index.js
@@ -0,0 +1,3 @@
+module.exports = require('./index.json')
+// el fichero json está cargado, así puede llamarse desde cualquier parte llamando a esta instancia, así no se producen múltiples llamadas a memoria diferentes
+// el json se guarda en el disco
